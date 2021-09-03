26e8f5154709ef5cfca0196bc43856b126e1c81c
helketari <helketari@comcast.net>
2021-09-03 04:07:15 -0400

    Created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">What's in a Name?</header>
+      <div className="container">
+        <header className="App-header">What's in a Name?</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="GitHub-link">Coded by Jessica Brosvic</footer>
+      </div>
     </div>
   );
 }
